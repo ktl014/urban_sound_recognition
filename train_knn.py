1875e1d9b9ccb4183b428fc3ddfc1891a77961d8
Kevin Le <ktl014@ucsd.edu>
2019-06-03 00:37:58 -0700

    train knn on 193 features

--- train_knn.py
+++ train_knn.py
@@ -18,45 +18,59 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix
 
-def mean_mfccs(x):
-    """Take average of MFCCs"""
-    return [np.mean(feature) for feature in librosa.feature.mfcc(x)]
+from data.dataloader import get_dataloader_v2
+from utils.eval_utils import dump_data
 
-def parse_audio(x):
-    """Parse each audio file for transformation"""
-    return x.flatten('F')[:x.shape[0]]
+"""BEGIN: Old feature extraction"""
+# def mean_mfccs(x):
+#     """Take average of MFCCs"""
+#     return [np.mean(feature) for feature in librosa.feature.mfcc(x)]
+#
+# def parse_audio(x):
+#     """Parse each audio file for transformation"""
+#     return x.flatten('F')[:x.shape[0]]
+#
+# def get_samples(block_size=2):
+#     """Get training inputs: spectrogram and labels"""
+#     # Static for quick development
+#     train_path = "dataset/UrbanSound8K/audio/fold1"
+#     train_file_names = os.listdir(train_path)
+#     # train_file_names = random.sample(train_file_names, 500)
+#
+#     samples = []
+#     labels = []
+#
+#     bad_count = 0
+#     for i,file_name in enumerate(train_file_names):
+#         if i % 50 == 0:
+#             print('Loaded {}/{}'.format(i, len(train_file_names)))
+#
+#         try:
+#             x, sr = librosa.load(os.path.join(train_path, file_name))
+#         except:
+#             bad_count += 1
+#             print("BAD AUDIO INPUT!! fn:{} {}".format(os.path.basename(
+#                 file_name), bad_count))
+#             continue
+#
+#         x = parse_audio(x)
+#         samples.append(mean_mfccs(x))
+#         labels.append(os.path.basename(file_name).split('-')[1])
+#
+#     return np.array(samples), np.array(labels)
+#
+# X, Y = get_samples()
+# x_train, x_test, y_train, y_test = train_test_split(X, Y)
+"""END: Old feature extraction"""
 
-def get_samples(block_size=2):
-    """Get training inputs: spectrogram and labels"""
-    # Static for quick development
-    train_path = "dataset/UrbanSound8K/audio/fold1"
-    train_file_names = os.listdir(train_path)
-    # train_file_names = random.sample(train_file_names, 500)
+# Grab data
+trainLoader, testLoader, _, data_cols = get_dataloader_v2(db_prepped=True)
 
-    samples = []
-    labels = []
+x_train = trainLoader[data_cols]
+y_train = trainLoader['label'].astype('category').cat.codes
 
-    bad_count = 0
-    for i,file_name in enumerate(train_file_names):
-        if i % 50 == 0:
-            print('Loaded {}/{}'.format(i, len(train_file_names)))
-
-        try:
-            x, sr = librosa.load(os.path.join(train_path, file_name))
-        except:
-            bad_count += 1
-            print("BAD AUDIO INPUT!! fn:{} {}".format(os.path.basename(
-                file_name), bad_count))
-            continue
-
-        x = parse_audio(x)
-        samples.append(mean_mfccs(x))
-        labels.append(os.path.basename(file_name).split('-')[1])
-
-    return np.array(samples), np.array(labels)
-
-X, Y = get_samples()
-x_train, x_test, y_train, y_test = train_test_split(X, Y)
+x_test = testLoader[data_cols]
+y_test = testLoader['label'].astype('category').cat.codes
 
 print(f'Shape: {x_train.shape}')
 print(f'Observation: \n{x_train[0]}')
@@ -83,8 +97,6 @@ grid_params = {
 model = GridSearchCV(KNeighborsClassifier(), grid_params, cv=5, n_jobs=-1)
 model.fit(x_train_scaled, y_train)
 
-print(f'Model Score: {model.score(x_test_scaled, y_test)}')
-
 y_predict = model.predict(x_test_scaled)
 cm =confusion_matrix(y_predict, y_test)
 cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
@@ -92,3 +104,14 @@ print(f'Confusion Matrix: \n{cm}')
 print(f'Class Accuracies')
 for acc in np.argsort(cm.diagonal()):
     print('Class {}: {}'.format(acc, cm.diagonal()[acc]))
+
+acc = model.score(x_test_scaled, y_test)
+print(f'Model Score: {acc}')
+
+# Store data
+name = 'KNN'
+acc_over_time = {}
+acc_over_time[name] = acc
+
+# Save data
+dump_data(acc_over_time)
